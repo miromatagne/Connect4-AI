6dc7b3c5f7ef58097d4b03854f829b29b1edbae2
mmatagne <miromanuel.matagne@gmail.com>
2021-06-03 23:09:27 +0200

    minor

--- game.py
+++ game.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     nb_Games = 1
     total_games_won = 0
     total_games_won2 = 0
-    #Change to True if one wants to play
-    want_to_play = False
+    # Change to True if one wants to play
+    want_to_play = True
 
     for i in range(nb_Games):
         game = Connect4Game(MINIMAX, MONTE_CARLO, iteration=300, depth1=4)
@@ -46,7 +46,8 @@ if __name__ == '__main__':
                 else:
                     for event in pygame.event.get():
                         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                            game.place(pygame.mouse.get_pos()[0] // SQUARE_SIZE)
+                            game.place(pygame.mouse.get_pos()
+                                       [0] // SQUARE_SIZE)
 
             elif ((game._turn == -1) and (game.get_win() is None)):
                 game.bot_place()
@@ -59,23 +60,28 @@ if __name__ == '__main__':
                     running = False
                 else:
                     running = False
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
     pygame.quit()
 
     if not want_to_play:
-        print("Win rate of Minimax: ", 100*(total_games_won/nb_Games), "%")  # Minimax
-    else: 
-        print("You won with a win rate of: ", 100*(total_games_won/nb_Games), "%")
-    print("Win rate of Monte Carlo: ", 100*(total_games_won2/nb_Games), "%")  # MonteCarlo
+        print("Win rate of Minimax: ", 100 *
+              (total_games_won/nb_Games), "%")  # Minimax
+    else:
+        print("You won with a win rate of: ",
+              100*(total_games_won/nb_Games), "%")
+    print("Win rate of Monte Carlo: ", 100 *
+          (total_games_won2/nb_Games), "%")  # MonteCarlo
 
-    
-
-    #Uncomment the following lines to create and train a deep neural network model
+    # Uncomment the following lines to create and train a deep neural network model
     # f = FileRecording()
     # f.generate_training_set('./Game_History_MM_vs_MC/')
 
-    #Uncomment the following lines to evaluate the model
+    # Uncomment the following lines to evaluate the model
     # nbOfGames = 100
     # evaluation = Evaluation("./model_Minmax_vs_MonteCarlo_3layers_no_duplicate")
     # percentage = evaluation.evaluate_model(nbOfGames, RANDOM_IMPR)
     # print(percentage)
-    
